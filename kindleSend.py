5f02a06c2f508c317f8e29c114ec4c728def3662
Adek06 <adek06@163.com>
2017-10-22 11:24:53 +0800

    自动记录登录信息

--- kindleSend.py
+++ kindleSend.py
@@ -1,15 +1,12 @@
 import os
 import smtplib
+import pickle
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
-SERVER = 'smtp.***.***'
 PORT = 25
-SENDER = "****@**.***"
-PASSWORD = "****"
-GETTER = "****@**.***"
-SUBJECT = "测试一下"
+SUBJECT = "convert"
 
 def addMIME(bookName, subject, sender, toEmail):
     filePath = str(os.getcwd())+"/kindleBooks/"+bookName
@@ -23,9 +20,43 @@ def addMIME(bookName, subject, sender, toEmail):
     msg['To'] = toEmail
     return msg.as_string()
 
+def pickleToDick(server, getter, sender, password):
+    d = dict(Server = server,Getter = getter,Sender = sender, Password = password )
+    with open('User.txt','wb') as f:
+        pickle.dump(d,f)        
+
+def login():
+    if('User.txt' in os.listdir()):
+        with open('./User.txt','rb') as f:
+            d = pickle.load(f)
+        if d != '':
+            print('使用帐号'+d['Sender']+'登录')
+            return d['Server'], d['Getter'], d['Sender'], d['Password']
+    else:
+        noLogin = True
+
+    while noLogin:
+        SERVER = input("请输入smtp服务器:")
+        GETTER = input("请输入kindle邮箱地址：")
+        SENDER = input("请输入发送人的邮箱地址：")
+        PASSWORD = input("请输入邮箱密码（该密码只会保存在本地，却不会以任何形式漏出）：")
+        send_mail = smtplib.SMTP(SERVER, 25)
+        send_mail.ehlo()
+        send_mail.starttls()
+        send_mail.ehlo() 
+        x = send_mail.login(SENDER, PASSWORD)
+        if x[0] < 500:
+            print("登录成功")
+            pickleToDick(SERVER, GETTER, SENDER, PASSWORD)
+            return SERVER, GETTER, SENDER, PASSWORD
+        else:
+            print("请重新确认信息")
+
+        
+
 def send(bookName, subject, server,port,sender,password,toEmail):
-    msg = addMIME(bookName, subject, sender, toEmail)
     try:
+        msg = addMIME(bookName, subject, sender, toEmail)
         send_mail = smtplib.SMTP(server,port)
         send_mail.ehlo()
         send_mail.starttls()
@@ -33,15 +64,16 @@ def send(bookName, subject, server,port,sender,password,toEmail):
         send_mail.login(sender,password)
         send_mail.sendmail(sender,toEmail,msg)
         send_mail.quit()
-        print("电子书发送成功！")
-    except:
-        print("发送过程有些问题！")
+        print(bookName+"发送成功！")
+    except smtplib.SMTPDataError:
+        print("你的邮件被当成了垃圾邮件，请不要重复发送过多电子书")
 def getBook():
     booklist = os.listdir("./kindleBooks")
     return booklist
 if __name__ == "__main__":
     #发送邮件
     booklist = getBook()
+    SERVER, GETTER, SENDER, PASSWORD = login()
     noEnd = True
     while(noEnd):
         print("你是要发送一下的书吗？Y/N")
